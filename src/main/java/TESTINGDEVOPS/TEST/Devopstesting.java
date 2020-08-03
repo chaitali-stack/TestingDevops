25a1a9ec15d90094b7b8d8112784ac058700453e
Sudhir Kale <chaitali.sudhir.kale@accenture.com>
2020-08-03 11:54:22 +0530

    my comments

---
+++ src/main/java/TESTINGDEVOPS/TEST/Devopstesting.java
@@ -0,0 +1,44 @@
+package TESTINGDEVOPS.TEST;
+
+import java.io.File;
+import java.awt.AWTException;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+
+
+import org.testng.annotations.BeforeTest;
+
+
+public class Devopstesting {
+
+
+	@Test(priority = 1)
+	public static void main(String arg[]) {
+	
+	System.out.println("Git code push checks");
+
+}
+
+}
+
+
